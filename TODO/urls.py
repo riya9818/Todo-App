9dfe01d724aefaa5123f20f5486b20d2070dee31
riya9818 <reeyabasnet999@gmail.com>
2025-08-31 22:46:59 +0545

    Todo Create and delete operations

--- TODO/urls.py
+++ TODO/urls.py
@@ -22,4 +22,6 @@ from todo_app import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", views.todo_list),
+    path("delete/<int:id>/", views.todo_delete),
+    path("create/", views.todo_create),
 ]
--- todo_app/views.py
+++ todo_app/views.py
@@ -1,3 +1,4 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
 
 from todo_app.models import Todo
@@ -10,3 +11,16 @@ def todo_list(request):
         "todo_list.html",
         {"todos":todos},
     )
+
+def todo_delete(request, id):
+    todo = Todo.objects.get(id=id)
+    todo.delete()
+    return HttpResponseRedirect("/")
+
+def todo_create(request):
+    print(request.method, request.POST)
+    if request.method == "GET":
+        return render(request, "todo_create.html")
+    else:
+        Todo.objects.create(title=request.POST["title"])
+        return HttpResponseRedirect("/")
